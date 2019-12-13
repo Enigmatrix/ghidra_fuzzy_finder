c882ddcca85eb2c9e5e14359cad72e41e341bf49
Enigmatrix <enigmatrix2000@gmail.com>
2019-12-13 19:50:20 +0800

    add fuzzy

---
+++ src/main/java/fuzzyfinder/FuzzyFinderDialog.java
@@ -0,0 +1,4 @@
+package fuzzyfinder;
+
+public class FuzzyFinderDialog {
+}
--- src/main/java/fuzzyfinder/FuzzyFinderPlugin.java
+++ src/main/java/fuzzyfinder/FuzzyFinderPlugin.java
@@ -1,39 +1,18 @@
-/* ###
- * IP: GHIDRA
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package fuzzyfinder;
 
-import java.awt.BorderLayout;
-
-import javax.swing.*;
 
 import docking.ActionContext;
-import docking.ComponentProvider;
 import docking.action.DockingAction;
-import docking.action.ToolBarData;
+import docking.action.MenuData;
 import ghidra.app.plugin.PluginCategoryNames;
 import ghidra.app.plugin.ProgramPlugin;
 import ghidra.framework.plugintool.*;
 import ghidra.framework.plugintool.util.PluginStatus;
-import ghidra.util.HelpLocation;
 import ghidra.util.Msg;
-import resources.Icons;
 
 @PluginInfo(
-	status = PluginStatus.STABLE,
-	packageName = "Fuzzy Finder",
+	status = PluginStatus.RELEASED,
+	packageName = "FuzzyFinder",
 	category = PluginCategoryNames.NAVIGATION,
 	shortDescription = "Fuzzy finder for Ghidra",
 	description = "Fuzzy finder for finding symbols, functions and labels in Ghidra"
@@ -41,70 +20,22 @@ import resources.Icons;
 //@formatter:on
 public class FuzzyFinderPlugin extends ProgramPlugin {
 
-	MyProvider provider;
+	public static String NAME = "Fuzzy Find";
 
-	/**
-	 * Plugin constructor.
-	 * 
-	 * @param tool The plugin tool that this plugin is added to.
-	 */
 	public FuzzyFinderPlugin(PluginTool tool) {
 		super(tool, true, true);
 
-		String pluginName = getName();
-		provider = new MyProvider(this, pluginName);
+		var triggerAction = new DockingAction(NAME, getName()) {
+			@Override
+			public void actionPerformed(ActionContext context) {
+				Msg.info(this, "Hello World!");
+			}
+		};
+		triggerAction.setEnabled(true);
+		triggerAction.setMenuBarData(new MenuData(new String[] { PluginCategoryNames.NAVIGATION, NAME}));
 
-		// TODO: Customize help (or remove if help is not desired)
-		String topicName = this.getClass().getPackage().getName();
-		String anchorName = "HelpAnchor";
-		provider.setHelpLocation(new HelpLocation(topicName, anchorName));
+		tool.addAction(triggerAction);
 	}
 
-	@Override
-	public void init() {
-		super.init();
 
-		// TODO: Acquire services if necessary
-	}
-
-	// TODO: If provider is desired, it is recommended to move it to its own file
-	private static class MyProvider extends ComponentProvider {
-
-		private JPanel panel;
-		private DockingAction action;
-
-		public MyProvider(Plugin plugin, String owner) {
-			super(plugin.getTool(), owner, owner);
-			buildPanel();
-			createActions();
-		}
-
-		// Customize GUI
-		private void buildPanel() {
-			panel = new JPanel(new BorderLayout());
-			JTextArea textArea = new JTextArea(5, 25);
-			textArea.setEditable(false);
-			panel.add(new JScrollPane(textArea));
-			setVisible(true);
-		}
-
-		// TODO: Customize actions
-		private void createActions() {
-			action = new DockingAction("My Action", getName()) {
-				@Override
-				public void actionPerformed(ActionContext context) {
-					Msg.showInfo(getClass(), panel, "Custom Action", "Hello!");
-				}
-			};
-			action.setToolBarData(new ToolBarData(Icons.ADD_ICON, null));
-			action.setEnabled(true);
-			action.markHelpUnnecessary();
-			dockingTool.addLocalAction(this, action);
-		}
-
-		@Override
-		public JComponent getComponent() {
-			return panel;
-		}
-	}
 }
